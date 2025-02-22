fd702b8ec87772554b8c2d5993017ee30178cecc
sofivillanueva <acostavillanuevasofia@gmail.com>
2025-02-22 17:46:36 -0300

    Agregue el callback de la segunda grafica, el resultado y etiquetas

--- src/components/GraficasCompletadas.jsx
+++ src/components/GraficasCompletadas.jsx
@@ -4,6 +4,12 @@ import React from "react";
 import Grafica from "./Grafica";
 import { useSelector } from "react-redux";
 
+/* Gráfico de minutos por actividad: se deberán graficar las
+actividades de las que hay registros mostrando la cantidad
+de sesiones de esa actividad, no se muestran en la gráfica las
+actividades que no tengan registros.
+*/
+
 const useActividades = () => {
   const listaActividades = useSelector((state) => state.sliceActividades);
 
@@ -37,17 +43,53 @@ const GraficasCompletadas = () => {
     return acc;
   };
 
-  const resultado = registros.reduce(callback, {});
+  const callbackMinutos = (acc, val) => { // FALTA, ver porque no se agregan los de exactamente hace una semana, y que se muestren todos los dias, no los que tienen solo registros
+    const fecha = new Date(val.fecha).toISOString().split("T")[0];
+
+    const hoy = new Date();
+    const semanaPasada = new Date();
+
+    semanaPasada.setDate(hoy.getDate() - 7);
+    console.log(semanaPasada)
+
+    if (new Date (val.fecha) >= semanaPasada ) {
+      const minutos = Number(val.tiempo) || 0; 
   
+      if (acc[fecha]) {
+        acc[fecha] += minutos; 
+      } else {
+        acc[fecha] = minutos; 
+      }
+    }
+    return acc;
+  };
+
+  const resultado = registros.reduce(callback, {});
+  const resultadoGraficaMinutos = registros.reduce(callbackMinutos, {});
+
   const idActividades = Object.keys(resultado);
   const datos = Object.values(resultado);
 
-  const etiquetas = idActividades.map(
-    id => convertirNombreActividad(id)
-  );
+  const etiquetas2 = Object.keys(resultadoGraficaMinutos).sort();
+  const datos2 = Object.values(resultadoGraficaMinutos);
+
+  const etiquetas = idActividades.map((id) => convertirNombreActividad(id));
 
   return (
-    <Grafica etiquetas={etiquetas} datos={datos} nombreGrafica="Sesiones por Actividad" nombreDatos="Cantidad de Sesiones" />
+    <div>
+      <Grafica
+        etiquetas={etiquetas}
+        datos={datos}
+        nombreGrafica="Sesiones por Actividad"
+        nombreDatos="Cantidad de Sesiones"
+      />
+      <Grafica
+        etiquetas={etiquetas2}
+        datos={datos2}
+        nombreGrafica="Minutos por día"
+        nombreDatos="Cantidad de minutos"
+      />
+    </div>
   );
 };
 
